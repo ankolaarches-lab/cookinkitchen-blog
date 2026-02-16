dba2150da9a54023652c9485eb292493f40429d5
Arches Ankola <archesankola@Archess-Mini.attlocal.net>
2026-02-16 10:50:26 -0800

    Fix regex flag

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -65,7 +65,7 @@ export default function ChatWidget() {
       .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
       .replace(/\*(.*?)\*/g, '<em>$1</em>')
       .replace(/^- (.+)$/gm, '<li>$1</li>')
-      .replace(/(<li>.*<\/li>)/s, '<ul>$1</ul>')
+      .replace(/(<li>[\s\S]*?<\/li>)/g, '<ul>$1</ul>')
       .replace(/\n\n/g, '<br/><br/>')
       .replace(/\n/g, '<br/>');
   };
